222329b2455da372dd0a51380b95d642bf6b3753
DiegoOno <diego.ono1505@gmail.com>
2020-01-13 23:15:42 -0300

    Implemented a Calculator with VueJS

---
+++ VueJS/calculator/src/App.vue
@@ -0,0 +1,44 @@
+// Component's template
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+</template>
+
+// Component's javascript
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+  components: { Calculator }
+}
+</script>
+
+// Component's CSS
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url(./fonts/RobotoMono-Thin.ttf);
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
